3a5dee252905aff6a7915111e0c2b7f31d9ca311
Sofía <124909933+a348451@users.noreply.github.com>
2023-05-26 23:53:40 -0600

    Se modifica InfoActivity.java

--- app/src/main/java/mx/uach/cv/InfoActivity.java
+++ app/src/main/java/mx/uach/cv/InfoActivity.java
@@ -3,6 +3,7 @@ package mx.uach.cv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -26,8 +27,10 @@ public class InfoActivity extends AppCompatActivity {
         String name = this.getIntent().getStringExtra("person");
         //AQUI LE METEMOS LOS TEXT VIEW
         final TextView title = findViewById(R.id.layout_title);
-
-        Logger.getLogger("app ->").info("===========================");
+        final TextView phone = findViewById(R.id.phone);
+        final TextView email = findViewById(R.id.email);
+        final TextView objective = findViewById(R.id.obj);
+        final TextView xp = findViewById(R.id.xp);
 
 
 
@@ -37,10 +40,9 @@ public class InfoActivity extends AppCompatActivity {
         title.setText(titleStr);
         Logger.getLogger("app ->").info(titleStr);
 
-
-        //YA NOMÁS FALTA METERLE QUE OBTENGA LOS DATOS DE INTERNET Y LUEGO MOSTRARLOS
         if(Objects.equals(titleStr, "gil")){
-            Logger.getLogger("app ->").info("===========================");
+            WebView webView = findViewById(R.id.photo);
+            webView.loadUrl("https://avatars.githubusercontent.com/u/24398402?v=4");
             RequestQueue queue = Volley.newRequestQueue(this);
             String url = "https://raw.githubusercontent.com/Gilelberto/info-cv-jsons/main/infoGil.json";
             StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -49,6 +51,11 @@ public class InfoActivity extends AppCompatActivity {
                     //System.out.println(response);
                     InfoCV infocv = new Gson().fromJson(response, InfoCV.class);
                     title.setText(infocv.name);
+                    phone.setText(infocv.phone);
+                    email.setText(infocv.email);
+                    objective.setText(infocv.objective);
+                    xp.setText(infocv.experience);
+
 
                 }
             }, new Response.ErrorListener(){
@@ -61,6 +68,8 @@ public class InfoActivity extends AppCompatActivity {
             queue.add(sr);
         }
         else if(Objects.equals(titleStr, "mau")){
+            WebView webView = findViewById(R.id.photo);
+            webView.loadUrl("https://cdn-5.motorsport.com/images/mgl/0a9neZP0/s1200/sergio-perez-red-bull-racing-1.webp");
             RequestQueue queue = Volley.newRequestQueue(this);
             String url = "https://raw.githubusercontent.com/LagMau2/cv2/master/cv_mau.json";
             StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -81,6 +90,8 @@ public class InfoActivity extends AppCompatActivity {
 
         }
         else if(Objects.equals(titleStr, "sof")){
+            WebView webView = findViewById(R.id.photo);
+            webView.loadUrl("https://avatars.githubusercontent.com/u/24398402?v=4");
             RequestQueue queue = Volley.newRequestQueue(this);
             String url = "https://raw.githubusercontent.com/a348451/shofo/main/shofocv.json";
             StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
